60280218738e5b01570af6453e716ef0aeb2d6e4
FeliciaCoding <49447678+FeliciaCoding@users.noreply.github.com>
2023-10-09 18:12:01 +0200

    Comments

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 /*
  * Subject : Lab 03 - Prime Numbers
  * Starting Date : 05.10.2023
- *
  */
 
 #include <iostream>
@@ -17,12 +16,11 @@ int main() {
     const int width  = 10;
 
 
-
-    restart_program:
+    restart_program: // The label of "goto"
     cout << "Ce programme ..." << endl;
     cout << endl;
 
-    // user input + input restrictions
+    // User input + input restrictions
     int value;
 
     do {
@@ -33,21 +31,21 @@ int main() {
     } while (value <= 1 or value > 1000);
 
 
-    // calculate prime numbers and display the results
+    // Calculate prime numbers and display the results
     cout << endl;
     cout << "Voici la liste des nombres premiers" << endl;
 
     int nb_prime;
     int number, divisor;
     int column_counter;
-    // int nb_time = 0; -> ERROR, have to put in the FOR loop, initial value = 0 for every iteration
+    // int nb_time = 0; -> [ERROR] have to put in the FOR loop, initial value = 0 for every iteration
 
             // Define prime numbers
             for (number = 1; number <= value; ++number) { // checking all numbers less than the value
 
                 int nb_time = 0;
 
-                //  calculate the number of times it is divisible
+                //  Calculate the number of times it is divisible
                 for (divisor = 1; divisor <= number; ++divisor) {
                     if (number % divisor == 0) {
                         ++nb_time;
@@ -67,7 +65,7 @@ int main() {
                             }
                         }
             }
-    // always display 1 empty line before start asking the following question
+    // Always display only 1 empty line before start asking the following question
     if (column_counter%column!=0){
         cout << endl;
     }
